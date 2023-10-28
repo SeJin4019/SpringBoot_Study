5f2e8dc4a1dfb53c6cbc408edb9f8348c93115bf
SeJin4019 <ksg4019113@naver.com>
2023-10-28 13:55:08 +0900

    Home 화면 추가

--- src/main/java/com/example/Recommendation/Controller/HomeController.java
+++ src/main/java/com/example/Recommendation/Controller/HomeController.java
@@ -10,4 +10,9 @@ public class HomeController {
     public String MainPage(){
         return "Main";
     }
+
+    @GetMapping("/Home")
+    public String HomePage(){
+        return "Home";
+    }
 }
